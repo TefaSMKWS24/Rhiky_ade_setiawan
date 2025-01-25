0953e0dda3f0ee6730b9fb5629d1d02ebe555287
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-01-25 07:50:24 -0800

    new file:   app/Http/Controllers/KasirController.php 	modified:   app/Http/Controllers/KategoriController.php 	modified:   app/Http/Controllers/PelangganController.php 	modified:   app/Http/Controllers/SupplierController.php 	modified:   app/Http/Controllers/TransaksiController.php 	new file:   resources/views/kasir/kasir.blade.php	new file:     resources/views/kategori/kategori.blade.php 	new file:   resources/views/pelanggan/index.blade.php 	new file:   resources/views/pelanggan/pelanggan.blade.php 	new file:   resources/views/supplier/supplier.blade.php 	new file:   resources/views/transaksi/transaksi.blade.php

--- app/Http/Controllers/PelangganController.php
+++ app/Http/Controllers/PelangganController.php
@@ -11,7 +11,7 @@ class PelangganController extends Controller
      */
     public function index()
     {
-        //
+        return view('pelanggan.index');
     }
 
     /**
--- app/Http/Controllers/TransaksiController.php
+++ app/Http/Controllers/TransaksiController.php
@@ -11,7 +11,7 @@ class TransaksiController extends Controller
      */
     public function index()
     {
-        //
+        return view('transaksi.index');
     }
 
     /**
