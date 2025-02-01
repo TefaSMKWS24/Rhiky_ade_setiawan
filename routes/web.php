d5cc663d0dcb9f8caf269084b71cea05378265f1
TefaSMKWS24 <tefaproject.smkws@gmail.com>
2025-02-01 10:32:00 -0800

    modified:   routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,16 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
+use App\http\controllers\GuestController;
+use App\http\controllers\AuthController;
 use App\Http\Controllers\BarangController;
 use App\Http\Controllers\KasirController;
 use App\Http\Controllers\kategoriController;
 use App\Http\Controllers\TransaksiController;
 use App\Http\Controllers\PelangganController;
+
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,8 +26,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('barang', BarangController::class);
-Route::resource('kasir', KasirController::class);
-Route::resource('kategori', kategoriController::class);
-Route::resource('transaksi', TransaksiController::class);
-Route::resource('pelanggan', PelangganController::class);
+
+// GUEST (sebelum login)
+Route::middleware(['guest:kasir'])->group(function () {
+ Route::get('/kasir', function () {return view('auth.loginkaisir');})->name('loginkasir');
+ Route::post('/loginkasir', [AuthController::class, 'loginkasir']);
+});
+
+Route::middleware(['guest:admin'])->group(function () {
+ Route::get('/admin', function () {return view('auth.loginadmin');})->name('loginadmin');
+ Route::post('/loginadmin', [AuthController::class, 'loginadmin']);
+});
+
+// AUTH (setelah login)
+ Route::middleware(['auth:kasir'])->group(function () {
+    Route::get('/kasir/dashboard', [DashbordkasirController::class, 'dashboard']);
+    Route::get('/kasir/logout', [AuthController::class, 'logoutkasir']);
+});
+
+ Route::middleware(['auth:admin'])->group(function () {
+    Route::get('/admin/dashboard', [DashbordadminController::class, 'dashboard']);
+    Route::get('/admin/logout', [AuthController::class, 'logoutadmin']);
+
+    Route::resource('barang', BarangController::class);
+    Route::resource('kasir', KasirController::class);
+    Route::resource('kategori', kategoriController::class);
+    Route::resource('transaksi', TransaksiController::class);
+    Route::resource('pelanggan', PelangganController::class);
+});
